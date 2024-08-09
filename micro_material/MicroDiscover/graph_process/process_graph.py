59c4b29efa85c3f57b4397d6d1131b50643bda7a
yfliu-Bryant <71213626+yfliu-Bryant@users.noreply.github.com>
2024-08-09 17:23:27 +0800

    Update process_graph.py

--- micro_material/MicroDiscover/graph_process/process_graph.py
+++ micro_material/MicroDiscover/graph_process/process_graph.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time : 2023/10/09 16:17
-# @Author : 刘宇峰
-# @File : prediction_model
-# @Software : PyCharm
-
-
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -20,10 +13,6 @@ from py2neo import Graph
 
 
 def graph_from_neo4j():
-    """
-    Use networkx to obtain graph data from the Neo4j knowledge graph, including node data and edge data
-    :return: Return the graph and data obtained from neo4j
-    """
     uri = "bolt://10.0.87.16:7687"
     username = "neo4j"
     password = "12345678"
@@ -67,10 +56,6 @@ def graph_from_neo4j():
 
 
 def nodes_ids_mapping(nodes_ids):
-    """
-    对图谱中的节点id进行映射
-    :return:映射的字典
-    """
     id_mapping = {}
     new_id = 0
     for node_id in nodes_ids:
@@ -80,10 +65,6 @@ def nodes_ids_mapping(nodes_ids):
 
 
 def lineage_microorganism_edges():
-    """
-    从微生物到发育树的路径
-    :return:
-    """
     uri = "bolt://10.0.87.16:7687"
     username = "neo4j"
     password = "12345678"
@@ -97,7 +78,6 @@ def lineage_microorganism_edges():
     """
     graph = nx.Graph()
 
-    # 执行查询并将结果添加到图中
     edges = []
     with driver.session() as session:
         result = session.run(query)
@@ -107,21 +87,15 @@ def lineage_microorganism_edges():
             edges.append((source_node, target_node))
     unique_edges = list(set(edges))
     csv_file = 'lineage_microorganism_edge.csv'
-    # 将数据写入 CSV 文件
     with open(csv_file, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['node1', 'node2'])  # 写入标题行
-        writer.writerows(unique_edges)  # 写入数据行
+        writer.writerow(['node1', 'node2'])  
+        writer.writerows(unique_edges)  
 
     driver.close()
 
 
 def graph_adjacency_matrix():
-    """
-    构造知识图谱的邻接矩阵
-    :param knowledge_graph: 构建好的知识图谱
-    :return: 知识图谱的邻接矩阵
-    """
     knowledge_graph = graph_from_neo4j()
     nodes_ids = list(knowledge_graph.nodes())
     nodes_num = len(knowledge_graph.nodes)
@@ -139,10 +113,6 @@ def graph_adjacency_matrix():
 
 
 def build_samples():
-    """
-    构造正负样本
-    :return: 返回样本
-    """
     adjacency_matrix = graph_adjacency_matrix()
 
     edge_index_pos = np.column_stack(np.argwhere(adjacency_matrix != 0))
@@ -150,24 +120,18 @@ def build_samples():
     edge_index_neg = np.column_stack(np.argwhere(adjacency_matrix == 0))
     edge_index_neg = torch.tensor(edge_index_neg, dtype=torch.long)
 
-    # 从正负样本中随机选取相同数量的边，形成平衡的训练集
     num_pos_edges_number = edge_index_pos.shape[1]
     selected_neg_edge_indices = torch.randint(high=edge_index_neg.shape[1], size=(num_pos_edges_number,),
                                               dtype=torch.long)
     edge_index_neg_selected = edge_index_neg[:, selected_neg_edge_indices]
     edg_index_all = torch.cat((edge_index_pos, edge_index_neg_selected), dim=1)
     y = torch.cat((torch.ones((edge_index_pos.shape[1], 1)),
-                   torch.zeros((edge_index_neg_selected.shape[1], 1))), dim=0)  # 将所有y值设置为1,0
-    # 将邻接矩阵转换为无向图（会将每条边复制成两条相反的边）
+                   torch.zeros((edge_index_neg_selected.shape[1], 1))), dim=0) 
     edge_index = to_undirected(edge_index_pos)
     return edge_index_pos, edge_index_neg_selected, edge_index, y
 
 
 def extract_data_from_neo4j():
-    """
-    从neo4j获取数据
-    :return:
-    """
     uri = "bolt://10.0.87.16:7687/browser/"
     username = "neo4j"
     password = "12345678"
@@ -180,10 +144,6 @@ def extract_data_from_neo4j():
 
 
 def build_nodes_features():
-    """
-    构造节点特征
-    :return: 返回构造好的节点特征
-    """
     knowledge_graph = graph_from_neo4j()
     node_features = np.zeros((len(knowledge_graph.nodes), 9))
     for i, (_, node_data) in enumerate(knowledge_graph.nodes(data=True)):
@@ -201,11 +161,6 @@ def build_nodes_features():
 
 
 def split_dataset(y):
-    """
-    将数据拆分为训练集和测试集
-    :param y: 数据集
-    :return: 返回训练集和测试集
-    """
     idx = np.arange(y.shape[0])
     np.random.shuffle(idx)
     train_idx = idx[:int(0.8 * len(idx))]
@@ -214,10 +169,6 @@ def split_dataset(y):
 
 
 def cypher_neo4j_samples():
-    """
-    使用cypher语言去查询知识图谱构造新的正负样本
-    :return:返回正负样本
-    """
     uri = "bolt://10.0.87.16:7687"
     username = "neo4j"
     password = "12345678"
@@ -253,10 +204,6 @@ def cypher_neo4j_samples():
 
 
 def microorganism_material_adjacency_matrix():
-    """
-    微生物-材料节点的邻接矩阵
-    :return: 返回微生物-材料的邻接矩阵
-    """
     uri = "bolt://10.0.87.16:7687"
     username = "neo4j"
     password = "12345678"
@@ -296,10 +243,6 @@ def microorganism_material_adjacency_matrix():
 
 
 def build_new_gcn_samples():
-    """
-    构造GCN和GAT模型的正负样本
-    :return: 返回正负样本数量
-    """
     # positive_triples, negative_triples, edges_with_id = cypher_neo4j_samples()
     adjacency_matrix = microorganism_material_adjacency_matrix()
     edge_index_pos = np.column_stack(np.argwhere(adjacency_matrix != 0))
@@ -307,58 +250,24 @@ def build_new_gcn_samples():
     edge_index_neg = np.column_stack(np.argwhere(adjacency_matrix == 0))
     edge_index_neg = torch.tensor(edge_index_neg, dtype=torch.long)
 
-    # 从正负样本中随机选取相同数量的边，形成平衡的训练集
     num_pos_edges_number = edge_index_pos.shape[1]
     selected_neg_edge_indices = torch.randint(high=edge_index_neg.shape[1], size=(num_pos_edges_number,),
                                               dtype=torch.long)
     edge_index_neg_selected = edge_index_neg[:, selected_neg_edge_indices]
     edg_index_all = torch.cat((edge_index_pos, edge_index_neg_selected), dim=1)
     y = torch.cat((torch.ones((edge_index_pos.shape[1], 1)),
-                   torch.zeros((edge_index_neg_selected.shape[1], 1))), dim=0)  # 将所有y值设置为1,0
-    # 将邻接矩阵转换为无向图（会将每条边复制成两条相反的边）
+                   torch.zeros((edge_index_neg_selected.shape[1], 1))), dim=0)
     edge_index = to_undirected(edge_index_pos)
     return edge_index_pos, edge_index_neg_selected, edge_index, y
 
 
-def search_microbe_accession(query):
-    """
-    查找 GenBank Accession 号码
-    :param query:需要查找的微生物
-    :return:
-    """
-    Entrez.email = "liuyf@cnic.cn"
-    handle = Entrez.esearch(db="nuccore", term=query, retmax=1)
-    record = Entrez.read(handle)
-    handle.close()
-    if 'IdList' in record and record['IdList']:
-        return record['IdList'][0]
-    elif 'WebEnv' in record and 'QueryKey' in record:
-        # 如果返回的是 WebEnv 和 QueryKey，则使用 efetch 获取 IdList
-        handle = Entrez.efetch(db="nuccore", webenv=record['WebEnv'], query_key=record['QueryKey'], retmax=1)
-        sub_record = Entrez.read(handle)
-        handle.close()
-
-        if 'IdList' in sub_record and sub_record['IdList']:
-            return sub_record['IdList'][0]
-
-    return None
-
-
 def get_meta_path():
-    """
-    获取知识图谱中所有的元路径实例
-    :return: 返回所有的元路径实例
-    """
-    # uri = "bolt://10.0.87.16:7687"
-    # username = "neo4j"
-    # password = "12345678"
 
     uri = "bolt://localhost:7687"
     username = "neo4j"
     password = "123456"
 
     driver = GraphDatabase.driver(uri, auth=(username, password))
-    # 获取元路径实例
     meta_path_types = [["lineage", "microorganism", "synthesis_method", "material", "element", "material"],
                        ['synthesis_method', 'microorganism', 'microorganism', 'synthesis_method']]
     paths = []
@@ -391,10 +300,6 @@ def get_meta_path():
 
 
 def generate_random_walk(meta_paths):
-    """
-    生成游走序列
-    :return:
-    """
     graph = graph_from_neo4j()
     start_node_type = ["microorganism", "synthesis_method"]
     start_node_list = []
@@ -405,8 +310,8 @@ def generate_random_walk(meta_paths):
         else:
             continue
 
-    walks_per_meta_path = 20  # 生成随机游走序列
-    walk_length = 50          # 每个游走序列的长度
+    walks_per_meta_path = 20 
+    walk_length = 50   
     all_walks = []
 
     for meta_path in start_node_list:
@@ -441,29 +346,6 @@ def generate_random_walk(meta_paths):
 
 
 def build_meta_path_samples():
-    """
-    构造meta_path正负样本
-    :return:
-    """
-    # uri = "bolt://10.0.87.16:7687"
-    # username = "neo4j"
-    # password = "12345678"
-    # driver = GraphDatabase.driver(uri, auth=(username, password))
-    # meta_path_types = [["microorganism", "synthesis_method", "material"]]
-    # meta_paths = []
-    # for meta_path in meta_path_types:
-    #     with driver.session() as session:
-    #         query = (
-    #             f"MATCH p = "
-    #             f"(A:{meta_path[0]})-[:CONNECTS]-"
-    #             f"(B:{meta_path[1]})-[:CONNECTS]-"
-    #             f"(C:{meta_path[2]})"
-    #             f"RETURN ID(A) as A_id, ID(B) AS B_id, ID(C) AS C_id"
-    #         )
-    #         result = session.run(query)
-    #         for path in result:
-    #             meta_paths.append(path)
-    # 构造正负样本
     knowledge_graph = graph_from_neo4j()
     nodes_list = list(knowledge_graph.nodes())
     id_mapping = nodes_ids_mapping(nodes_list)
@@ -473,14 +355,13 @@ def build_meta_path_samples():
     test_negative_samples = []
     df = pd.read_csv("../kg/output_file.csv")
     name_to_id = {node['name']: node_id for node_id, node in knowledge_graph.nodes(data=True)}
-    # 根据'name1'和'name2'的值获取对应节点的ID
     df['name1_id'] = df['microorganism_name'].map(name_to_id)
     df['name2_id'] = df['material_name'].map(name_to_id)
     df['name3_id'] = df['synthesis_name'].map(name_to_id)
     df.dropna(subset=['name1_id'], inplace=True)
 
-    train_data = df[df["year"] < 2020][["name1_id", "name2_id", "name3_id"]].values.tolist()
-    test_data = df[df["year"] >= 2020][["name1_id", "name2_id", "name3_id"]].values.tolist()
+    train_data = df[df["year"] < 2019][["name1_id", "name2_id", "name3_id"]].values.tolist()
+    test_data = df[df["year"] >= 2019][["name1_id", "name2_id", "name3_id"]].values.tolist()
 
     defineName_microorganism = find_nodes_by_define_name(knowledge_graph, "microorganism")
     print(len(defineName_microorganism))
@@ -533,11 +414,7 @@ def build_meta_path_samples():
 
 
 def build_samples_MRR():
-    """
-    构造MRR数据集
-    :return:
-    """
-    # 构造正负样本
+
     knowledge_graph = graph_from_neo4j()
     nodes_list = list(knowledge_graph.nodes())
     id_mapping = nodes_ids_mapping(nodes_list)
@@ -547,7 +424,6 @@ def build_samples_MRR():
     test_negative_samples = []
     df = pd.read_csv("../kg/output_file.csv")
     name_to_id = {node['name']: node_id for node_id, node in knowledge_graph.nodes(data=True)}
-    # 根据'name1'和'name2'的值获取对应节点的ID
     df['name1_id'] = df['microorganism_name'].map(name_to_id)
     df['name2_id'] = df['material_name'].map(name_to_id)
     df.dropna(subset=['name1_id'], inplace=True)
@@ -595,18 +471,14 @@ def build_samples_MRR():
 
 
 def read_test_positive_samples():
-    """
-    读取到测试集中的正样本
-    :return: 测试集中的正样本
-    """
     df = pd.read_csv("../kg/output_file.csv")
-    # 根据'name1'和'name2'的值获取对应节点的ID
+
     df['name1_id'] = df['microorganism_name']
     df['name2_id'] = df['material_name']
     df.dropna(subset=['name1_id'], inplace=True)
 
-    # train_data = df[df["year"] < 2020][["name1_id", "name2_id"]].values.tolist()
-    test_data = df[df["year"] >= 2020][["name1_id", "name2_id"]].values.tolist()
+    train_data = df[df["year"] < 2019][["name1_id", "name2_id"]].values.tolist()
+    test_data = df[df["year"] >= 2019][["name1_id", "name2_id"]].values.tolist()
     return test_data
 
 
@@ -614,49 +486,32 @@ def find_nodes_by_define_name(graph, value):
     nodes = []
     for node in graph.nodes(data=True):
         if 'defineName' in node[1] and node[1]['defineName'] == value:
-            # 如果节点满足条件，则将其添加到结果列表中
-            nodes.append(node[0])  # node[0] 是节点的 ID
+            nodes.append(node[0]) 
     return nodes
 
 
 def get_name_by_id(graph, node_id):
-    # 检查图中是否包含指定 ID 的节点
+
     if node_id in graph.nodes:
-        # 获取节点对象
+
         node = graph.nodes[node_id]
-        # 检查节点对象中是否包含 'name' 属性
+
         if 'name' in node:
-            # 返回节点的 'name' 属性值
             return node['name']
         else:
-            return None  # 如果节点不包含 'name' 属性，则返回 None
+            return None 
     else:
-        return None  # 如果图中不包含指定 ID 的节点，则返回 None
+        return None  
 
 
 def samples_by_year():
-    """
-    按年份划分正负样本
-    :return: 返回正负样本
-    """
+
     material_information_df = pd.read_csv("../kg/material_information.csv", usecols=['name', 'doi'])
     microorganism_information_df = pd.read_csv("../kg/microorganism_information.csv", usecols=['name', 'doi'])
     merged_df = pd.merge(material_information_df, microorganism_information_df, on='doi', how='inner')
     print(merged_df.head())
-    # 打印合并后的DataFrame
-    # print(merged_df)
-    # for doi_item in doi_list:
-    #     url = f'https://api.crossref.org/works/{doi_item}'
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     publication_date = data['message']['created']['date-time']
-    #     return publication_date
-    # else:
-    #     return None
 
 
-# 定义模型
 class Classification(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(Classification, self).__init__()
